dbaa55e1a10fb50d101e9eafc05c24556470e2cd
Sirin <ahmet.sirin@piri1513.com.tr>
2024-11-08 23:47:13 +0300

    fix

--- src/components/mainSection/index.jsx
+++ src/components/mainSection/index.jsx
@@ -14,30 +14,30 @@ function MainSection() {
                 <a className="btn ml-3 main-section-sign-up" href="#sign-up">Sign Up Now</a>
                 <a className="btn ml-3 main-section-run-video" href="#run-video">
                   {/* <i className="bi bi-play-circle"></i> */}
-                  <img className="main-section-watch-demo" src="/images/mainSection/watch-demo.png"></img>
+                  <img className="main-section-watch-demo" src="/landingPage10/images/mainSection/watch-demo.png"></img>
                   &nbsp; Watch Demo
                 </a>
               </div>
             </div>
             <div className="main-section-col-2 col-md-6">
               <div className='main-section-col-2-box'>
-                <img className='main-section-image' src="/images/mainSection/shoe.png" alt="Shoe" />
+                <img className='main-section-image' src="/landingPage10/images/mainSection/shoe.png" alt="Shoe" />
               </div>
             </div>
           </div>
           <div className="row main-section-row-2">
             <div className="col-md-4">
-              <img className='main-section-row2-image' src="/images/mainSection/row2col1.1.png" alt="Shoe" />
+              <img className='main-section-row2-image' src="/landingPage10/images/mainSection/row2col1.1.png" alt="Shoe" />
               <h3>Nibh vierra</h3>
               <p>Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa.</p>
             </div>
             <div className="col-md-4">
-              <img className='main-section-row2-image' src="/images/mainSection/row2col2.1.png" alt="Shoe" />
+              <img className='main-section-row2-image' src="/landingPage10/images/mainSection/row2col2.1.png" alt="Shoe" />
               <h3>Cursus amet</h3>
               <p>Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa.</p>
             </div>
             <div className="col-md-4">
-              <img className='main-section-row2-image' src="/images/mainSection/row2col3.1.png" alt="Shoe" />
+              <img className='main-section-row2-image' src="/landingPage10/images/mainSection/row2col3.1.png" alt="Shoe" />
               <h3>Ipsum fermentum</h3>
               <p>Sit bibendum donec dolor fames neque vulputate non sit aliquam. Consequat turpis natoque leo, massa.</p>
             </div>
