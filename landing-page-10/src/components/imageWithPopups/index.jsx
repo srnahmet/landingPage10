df10f59c20df55be4d9bd1de6af92837032aa497
Sirin <ahmet.sirin@piri1513.com.tr>
2024-11-08 22:52:32 +0300

    section6

--- landing-page-10/src/components/imageWithPopups/index.jsx
+++ landing-page-10/src/components/imageWithPopups/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './ImageWithPopups.css';
 
 function ImageWithPopups() {
   return (
-    <div>ImageWithPopups</div>
-  )
+    <div className="image-container">
+      <img src="/images/imageWithPopups/map.png" alt="Background" className="background-image" />
+
+      <h1 className="centered-title">11,658,467</h1>
+      <h2 className="centered-title-2">Shoes Collected</h2>
+
+      <div className="popup">
+        <img src="/images/imageWithPopups/popup.png" alt="Popup Content" className="popup-image" />
+
+        <div className="popup-extension">
+          <p className="extension-text">Emma Simpson collected one pair of Cool Shoes.</p>
+          <div className="arrow-down"></div> 
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ImageWithPopups
+export default ImageWithPopups;
